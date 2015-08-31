38e355ea1bab46be5be34d9d9b1f17f68f0b4dff
Toru Mitsutake <mitsumitsu.zikkyou@gmail.com>
2015-08-31 20:30:11 +0900

    firstcommit

---
+++ SerialSoft/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿using System.IO.Ports;
+
+namespace SerialSoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(618, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(249, 27);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(540, 300);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 423);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "シリアル通信プログラム";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SerialSoft/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialSoft
+{
+    public partial class Form1 : Form
+    {
+        private class comboBox2item : Object
+        {
+            private string m_name = "";
+            private int m_value = 0;
+
+            // 表示名称
+            public string NAME
+            {
+                set { m_name = value; }
+                get { return m_name; }
+            }
+
+            // ボーレート設定値.
+            public int BAUDRATE
+            {
+                set { m_value = value; }
+                get { return m_value; }
+            }
+
+            // コンボボックス表示用の文字列取得関数.
+            public override string ToString()
+            {
+                return m_name;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //! シリアルポートをオープンしている場合、クローズする.
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            //! シリアルポートをオープンしていない場合、処理を行わない.
+            if (serialPort1.IsOpen == false)
+            {
+                return;
+            }
+
+            try
+            {
+                //! 受信データを読み込む.
+                string data = serialPort1.ReadExisting();
+
+                //! 受信したデータをテキストボックスに書き込む.
+                Invoke(new Delegate_RcvDataToTextBox(RcvDataToTextBox), new Object[] { data });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private delegate void Delegate_RcvDataToTextBox(string data);
+        private void RcvDataToTextBox(string data)
+        {
+            //! 受信データをテキストボックスの最後に追記する.
+            if (data != null)
+            {
+                textBox1.AppendText(data);
+            }
+        }
+
+        
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+
+            if (serialPort1.IsOpen == true)
+            {
+                //! シリアルポートをクローズする.
+                serialPort1.Close();
+
+                //! ボタンの表示を[切断]から[接続]に変える.
+                button1.Text = "接続";
+            }
+            else
+            {
+                //! オープンするシリアルポートをコンボボックスから取り出す.
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+
+                //! ボーレートをコンボボックスから取り出す.
+                comboBox2item baud = (comboBox2item)comboBox2.SelectedItem;
+                serialPort1.BaudRate = baud.BAUDRATE;
+
+                //! データビットをセットする. (データビット = 8ビット)
+                serialPort1.DataBits = 8;
+
+                //! パリティビットをセットする. (パリティビット = なし)
+                serialPort1.Parity = Parity.None;
+
+                //! ストップビットをセットする. (ストップビット = 1ビット)
+                serialPort1.StopBits = StopBits.One;
+
+                //! フロー制御をコンボボックスから取り出す.
+                //! 文字コードをセットする.
+                serialPort1.Encoding = Encoding.ASCII;
+
+                try
+                {
+                    //! シリアルポートをオープンする.
+                    serialPort1.Open();
+
+                    //! ボタンの表示を[接続]から[切断]に変える.
+                    button1.Text = "切断";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //! 利用可能なシリアルポート名の配列を取得する.
+            string[] PortList = SerialPort.GetPortNames();
+
+            comboBox1.Items.Clear();
+
+            //! シリアルポート名をコンボボックスにセットする.
+            foreach (string PortName in PortList)
+            {
+                comboBox1.Items.Add(PortName);
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+
+            comboBox2.Items.Clear();
+
+            // ボーレート選択コンボボックスに選択項目をセットする.
+            comboBox2item baud;
+            baud = new comboBox2item();
+            baud.NAME = "4800bps";
+            baud.BAUDRATE = 4800;
+            comboBox2.Items.Add(baud);
+
+            baud = new comboBox2item();
+            baud.NAME = "9600bps";
+            baud.BAUDRATE = 9600;
+            comboBox2.Items.Add(baud);
+
+            baud = new comboBox2item();
+            baud.NAME = "19200bps";
+            baud.BAUDRATE = 19200;
+            comboBox2.Items.Add(baud);
+
+            baud = new comboBox2item();
+            baud.NAME = "115200bps";
+            baud.BAUDRATE = 115200;
+            comboBox2.Items.Add(baud);
+            comboBox2.SelectedIndex = 1;
+            
+
+            // フロー制御選択コンボボックスに選択項目をセットする.
+
+            // 送受信用のテキストボックスをクリアする.
+            textBox1.Clear();
+        }
+    }
+
+
+
+
+}
